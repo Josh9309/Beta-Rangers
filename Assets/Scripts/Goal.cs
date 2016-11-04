d68db46ca51560d653d6dc6de537c49a6529611d
Noah Parker <ndp4918@ad.rit.edu>
2016-11-04 13:27:50 -0400

    World Controller Setup and Other things
    - Implemented all attributes, properites, and start of methods for
    worldController outlined in class/Script doc
    - implemented all attributes, properites, and start of methods for key
    outlined in class/Script doc
    -  implemented all attributes, properites, and start of methods for goal
    outlined in class/Script doc
    - Created world controller initual prefab
    - Created Key prefab
    - Created Goal prefab

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -3,6 +3,17 @@ using System.Collections;
 
 public class Goal : MonoBehaviour {
 
+    ///attributes
+    private Color rangerColor;
+    private Rigidbody2D rBody;
+
+    ///Properties
+    public Color RangerColor
+    {
+        get { return rangerColor; }
+        set { rangerColor = value; }
+    }
+
 	// Use this for initialization
 	void Start () {
 	
--- Assets/Scripts/WorldController.cs
+++ Assets/Scripts/WorldController.cs
@@ -2,9 +2,76 @@
 using System.Collections;
 
 public class WorldController : MonoBehaviour {
+    
+    ///attributes
+    public enum cMenu { MAINMENU, OPTIONS, PAUSE, BATTLE};
+    public enum cLevel { NULL };
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField] private cMenu currentMenu;
+    [SerializeField] private cLevel currentLevel;
+    private Player player1;
+    private Player player2;
+    private Player player3;
+    private Player player4;
+    [SerializeField] private GameObject prefabRangerRed;
+    [SerializeField] private GameObject prefabRangerBlue;
+    [SerializeField] private GameObject prefabRangerYellow;
+    [SerializeField] private GameObject prefabRangerGreen;
+    [SerializeField] private GameObject prefabRangerBlack;
+    [SerializeField] private GameObject prefabRangerPink;
+    [SerializeField] private GameObject prefabGoal;
+    private GameObject goalP1;
+    private GameObject goalP2;
+    private GameObject goalP3;
+    private GameObject goalP4;
+    [SerializeField] private int keyMaxTime;
+
+    ///properties
+    public cMenu CurrentMenu{
+        get { return currentMenu; }
+        set { currentMenu = value; }
+    }
+    public cLevel CurrentLevel
+    {
+        get { return currentLevel; }
+        set { currentLevel = value; }
+    }
+    public Player Player1
+    {
+        get { return player1; }
+        set { player1 = value; }
+    }
+    public Player Player2
+    {
+        get { return player2; }
+        set { player2 = value; }
+    }
+    public Player Player3
+    {
+        get { return player3; }
+        set { player3 = value; }
+    }
+    public Player Player4
+    {
+        get { return player4; }
+        set { player4 = value; }
+    }
+
+    public int KeyMaxTime
+    {
+        get { return keyMaxTime; }
+    }
+
+    ///Methods
+
+    //called when the script instance is being loaded.
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
@@ -12,4 +79,14 @@ public class WorldController : MonoBehaviour {
 	void Update () {
 	
 	}
+
+    void win(Player playerWhoWon)
+    {
+
+    }
+
+    void levelSelect()
+    {
+
+    }
 }
