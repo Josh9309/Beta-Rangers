5c0be0e3c34cb2cbc15e89c79cd389e8b43ff225
Robert Anthony Santiago <ras1038@g.rit.edu>
2016-11-06 21:03:14 -0500

    MainMenuAdded
    Added Menu System, including Main Menu and Options pages and an Exit
    button

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void NewGameButton (string newGameLevel)
+    {
+        SceneManager.LoadScene(newGameLevel);
+    }
+
+    public void ExitGameButton()
+    {
+        Application.Quit();
+    }
+}
