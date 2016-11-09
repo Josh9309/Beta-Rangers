fa109329e653044e47fd9cfb51e0bb8919c4edc4
Josh Malmquist <joshmalmquist96@gmail.com>
2016-11-09 08:04:44 -0500

    Started work on Blue rangers attack

--- Assets/Scripts/BlueRanger.cs
+++ Assets/Scripts/BlueRanger.cs
@@ -4,6 +4,30 @@ using System;
 
 public class BlueRanger : Player {
 
+    #region Attributes
+    [SerializeField] private GameObject IceShurikenPrefab;
+    private int shurikenMax; //max amount of shurikens allowed to be thrown at once on screen
+    private int shurikenCount = 0; //current shurikens on the screen
+    #endregion
+
+    #region Properties
+    public int ShurikenCount
+    {
+        get { return shurikenCount; }
+        set
+        {
+            if (value < 0)
+            {
+                shurikenCount = 0;
+            }
+            else
+            {
+                shurikenCount = value;
+            }
+        }
+    }
+    #endregion
+
     // Use this for initialization
     protected override void Start()
     {
@@ -21,11 +45,29 @@ public class BlueRanger : Player {
         input.ResetBtns();
     }
 
+    //ICE SHURIKEN ATTACK
     protected override void Attack2()
     {
-        throw new NotImplementedException();
+        if(input.attack2 && shurikenCount < shurikenMax) //checks to make sure button has been pressed and that we have not hit max shurikens on the screen
+        {
+            IceShuriken shurikenScript;
+            GameObject iceShuriken;
+
+            if (facingLeft)
+            {
+                iceShuriken = Instantiate(IceShurikenPrefab, new Vector3(gameObject.transform.position.x - 2, gameObject.transform.position.y), Quaternion.identity) as GameObject;
+                shurikenScript = iceShuriken.GetComponent<IceShuriken>();
+
+            }
+            else
+            {
+                iceShuriken = Instantiate(IceShurikenPrefab, new Vector3(gameObject.transform.position.x + 2, gameObject.transform.position.y), Quaternion.identity) as GameObject;
+                shurikenScript = iceShuriken.GetComponent<IceShuriken>();
+            }
+        }
     }
 
+    //ICE FREEZE ATTACK
     protected override void SuperAttack()
     {
         throw new NotImplementedException();
---
+++ Assets/Scripts/IceShuriken.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IceShuriken : MonoBehaviour {
+    #region Attributes
+    private float speed = 5.0f;
+    public bool moveLeft = true;
+    private int damage;
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
