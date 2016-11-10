766a923b6a5006dbe2daf2af482f8a850f387b01
Robert Anthony Santiago <ras1038@g.rit.edu>
2016-11-10 14:59:20 -0500

    MenuAdded

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void NewGameButton(string newGameLevel)
+    {
+        Application.LoadLevel(newGameLevel);
+    }
+
+    public void ExitGameButton()
+    {
+        Application.Quit();
+    }
+}
