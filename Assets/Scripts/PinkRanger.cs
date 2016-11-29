0293399f297637a06fab1b57b3e0982b7d5e52e0
Noah Parker <ndp4918@ad.rit.edu>
2016-11-28 21:53:46 -0500

    started poison cloud
    - added class
    - prefab
    - pink ranger super attack method

--- Assets/Scripts/PinkRanger.cs
+++ Assets/Scripts/PinkRanger.cs
@@ -8,6 +8,9 @@ public class PinkRanger : Player {
     [SerializeField] private GameObject PoisonPrefab;
     [SerializeField] private float StatDartVelocity;
     [SerializeField] private float statTimeMax;
+    [SerializeField] private float poisonCloudSpeed; //How fast the black hole moves to point
+    [SerializeField] private float poisonCloudMoveTime; //the amount of time the black hole moves for
+    [SerializeField] private float poisonCloudStillTime; //the amount of time the black hole stays still for
 
     // Use this for initialization
     protected override void Start()
@@ -47,6 +50,15 @@ public class PinkRanger : Player {
 
     protected override void SuperAttack()
     {
-        
+        if (input.attack3 && SuperCurrent >= SuperCost)
+        {
+            GameObject a = GameObject.Instantiate(StatDartPrefab) as GameObject;
+            GameObject b = GameObject.Instantiate(StatDartPrefab) as GameObject;
+            a.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            b.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            a.GetComponent<PoisonCloud>().startUp(playerNum, true, poisonCloudSpeed, poisonCloudMoveTime, poisonCloudStillTime);
+            b.GetComponent<PoisonCloud>().startUp(playerNum, false, poisonCloudSpeed, poisonCloudMoveTime, poisonCloudStillTime);
+            SuperCurrent -= SuperCost;
+        }
     }
 }
---
+++ Assets/Scripts/poisonCloud.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoisonCloud : MonoBehaviour {
+
+    private int playerNum;//owners player number
+    private bool moving = true;
+    private float moveTimeCurrent; //current time has been moving
+    private float moveTimeMax; //how long black hole moves for
+    private float stillTimeCurrent;//current time has been still
+    private float stillTimeMax;//max time the black hole can stay still for before deleting
+    private Rigidbody2D rBody;
+    protected WorldController worldControl;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void startUp(int pNum, bool direction, float speed, float time, float stillTime)
+    {
+        worldControl = GameObject.FindGameObjectWithTag("Game Manager").GetComponent<WorldController>();
+        rBody = GetComponent<Rigidbody2D>();
+        playerNum = pNum;
+        moveTimeMax = time;
+        stillTimeMax = stillTime;
+        if (direction)//left
+        {
+            rBody.velocity = new Vector2(-speed, 0);
+        }
+        else//right
+        {
+            rBody.velocity = new Vector2(speed, 0);
+        }
+    }
+}
