a315cefba38ae3fe285c3ec63a8c7118d273b42c
Robert Anthony Santiago <ras1038@g.rit.edu>
2016-12-15 17:05:15 -0500

    AddedPauseScreen

---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseScript : MonoBehaviour {
+
+    public Transform pauseScreen;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+
+
+    public void Pause()
+    {
+
+            if(pauseScreen.gameObject.activeInHierarchy == false)
+            {
+                pauseScreen.gameObject.SetActive(true);
+                Time.timeScale = 0;
+            }
+
+            else
+            {
+                pauseScreen.gameObject.SetActive(false);
+                Time.timeScale = 1;
+            }
+    }
+
+    public void Exit()
+    {
+        Application.LoadLevel("MainScene");
+    }
+}
